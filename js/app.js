ff2a004c0f408b0bea6e8ab33a4b7f9eba8b26ac
Adeniyi Tofunmi <Adeniyitofunmi@gmail.com>
2021-12-23 05:55:14 +0100

    feat: the calculator now works

--- js/app.js
+++ js/app.js
@@ -6,26 +6,27 @@ const billField = document.getElementById('bill');
 const peopleField = document.getElementById('people');
 // The grid area for the tip buttons on the page
 const gridArea = document.querySelector('.grid');
-
-gridArea.addEventListener('click', function(e){
-  console.log(e.target);
-  e.target.classList.toggle('selected');
-})
+// The custom tip input
+const customTip = document.getElementById("custom-input")
+// The total tip amount
+const totalTip = document.querySelector("#tip-amount")
+// The total amount per person
+const totalAmount = document.querySelector("#total-amount");
 
 
 billField.addEventListener("keyup", function () {
   // This is to enable or disable the reset button
   if (billField.value === "") {
-    resetBtn.setAttribute('disabled', " ");
+    resetBtn.setAttribute('disabled', "");
   } else {
     resetBtn.removeAttribute("disabled");
   }
 })
 
-
-function calculateTip(tip){
+// Calculating the tip function
+function calculateTip(tip=10){
   let billAmount = billField.value === "" ? "error" : billField.value;
-  let peopleNum = peopleField.value === "" ? "error": peopleField.value;
+  let peopleNum = peopleField.value === "" ? "error" : peopleField.value;
 
   if(billAmount === "error" || peopleNum === "error"){
     return "error";
@@ -34,6 +35,20 @@ function calculateTip(tip){
   }
 }
 
+// clearing the tip calculator
 resetBtn.addEventListener("click", e =>{
-  console.log(calculateTip(15))
+  totalAmount.textContent = "0.00"
+  totalTip.textContent = "0.00"
+})
+
+// Calculating the tip amount after selecting the tip %
+gridArea.addEventListener("click", function (e) {
+  totalAmount.textContent = Math.floor(calculateTip(e.target.value));
+  totalTip.textContent = Math.floor(Math.floor(calculateTip(e.target.value)) / peopleField.value)
+});
+
+// calculating the tip amount after a custom tip %
+customTip.addEventListener("keyup", function(e){
+  totalAmount.textContent = Math.floor(calculateTip(e.target.value));
+  totalTip.textContent = Math.floor(Math.floor(calculateTip(e.target.value)) / peopleField.value)
 })
